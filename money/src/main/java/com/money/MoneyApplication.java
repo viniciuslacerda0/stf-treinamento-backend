02fbe7fc0c1de295bfd8f963b0370daf12cf988d
José Vinícius Lacerda de Arruda <jose.arruda@ccc.ufcg.edu.br>
2019-12-05 20:59:10 -0300

    Add files via upload

--- money/src/main/java/com/money/MoneyApplication.java
+++ money/src/main/java/com/money/MoneyApplication.java
@@ -2,6 +2,11 @@ package com.money;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class MoneyApplication {
@@ -10,4 +15,18 @@ public class MoneyApplication {
 		SpringApplication.run(MoneyApplication.class, args);
 	}
 
+	
+	@SuppressWarnings("rawtypes")
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        @SuppressWarnings("unchecked")
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+
+        return bean;
+	}
 }
